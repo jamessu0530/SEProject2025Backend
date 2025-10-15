62c3043306fcb6715faa10718b96e84fc535ac57
York <york4818@gmail.com>
2025-10-15 10:00:43 +0800

    新增service層

---
+++ src/main/java/com/ntou/auctionSite/service/UserService.java
@@ -0,0 +1,11 @@
+package com.ntou.auctionSite.service;
+
+import com.ntou.auctionSite.model.User;
+
+public interface UserService {
+    // 登入功能
+    User loginService(String id, String password);
+
+    //註冊功能
+    User registerService(User user);
+}
---
+++ src/main/java/com/ntou/auctionSite/service/serviceImpl/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package com.ntou.auctionSite.service.serviceImpl;
+
+import com.ntou.auctionSite.model.User;
+import com.ntou.auctionSite.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public User loginService(String id, String password) {
+        return null;
+    }
+
+    @Override
+    public User registerService(User user) {
+        return null;
+    }
+}
