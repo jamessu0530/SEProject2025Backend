93b5c2945d48d718e7cde3943ad81f109508bd4f
York <york4818@gmail.com>
2025-11-13 17:41:29 +0800

    移除Cart 資料結構裡的業務邏輯

--- src/main/java/com/ntou/auctionSite/model/Cart.java
+++ src/main/java/com/ntou/auctionSite/model/Cart.java
@@ -1,68 +1,23 @@
 package com.ntou.auctionSite.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.ArrayList;
+import java.util.List;
 
-
+@Data
+@NoArgsConstructor 
 public class Cart {
-    private ArrayList <Products> products;
-    private class Products {
-        String ProductID;
-        int Quantity;
-        public Products(String productID) {
-            this.ProductID = productID;
-            this.Quantity = 1;
-        }
-        public String getProductID() { return ProductID; }
-        public void setProductID(String productID) { ProductID = productID; }
-        public int getQuantity() { return Quantity; }
-        public void setQuantity(int quantity) { Quantity = quantity; }
-    }
+    private List<CartItem> items = new ArrayList<>();
 
-    public Cart() {
-        this.products = new ArrayList<Products>();
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class CartItem {
+        private int Quantity;
+        private String productID;
     }
-    //取得購物車內容
-    public ArrayList<Products> getProducts() { return products; }
-    //加入購物車
-    public void addProduct(String productID) {
-        for (Products p : products) {
-            if (p.getProductID().equals(productID)) {
-                p.setQuantity(p.getQuantity() + 1);
-                return;
-            }
-        }
-        products.add(new Products(productID));
-    }
-    //加數量
-    public void quantityIncrease(String productID) {
-        for (Products p : products) {
-            if (p.getProductID().equals(productID)) {
-                p.setQuantity(p.getQuantity() + 1);
-                return;
-            }
-        }
-    }
-    //減數量
-    public void quantityDecrease(String productID) {
-        for (Products p : products) {
-            if (p.getProductID().equals(productID)) {
-                if (p.getQuantity() > 1) {
-                    p.setQuantity(p.getQuantity() - 1);
-                } else {
-                    products.remove(p);
-                }
-                return;
-            }
-        }
-    }
-    //移除商品
-    public void removeProduct(String productID) {
-        products.removeIf(p -> p.getProductID().equals(productID));
-    }
-    //清空購物車
-    public void clearCart() {
-        products.clear();
-    }
-
 }
 
